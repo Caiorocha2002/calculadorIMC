10ac64757f9446570d814fa10af2f2ab8911bfc0
Caio Rocha <caiorochauba@outlook.com>
2022-03-26 17:43:59 -0300

    adicionei os arquivos

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+ 
+
+
+ function result(){
+   
+     var heightElement= document.getElementById("height");
+      var height= heightElement.value;
+
+       var weightElement= document.getElementById("weight");
+       var weight= weightElement.value;
+ 
+     var calculation= weight/(height*height);
+     var imc = parseInt(calculation);
+    
+     var elementValueResult= document.getElementById("result");
+     var resultImc= " O valor do resultado do seu calculo IMC  é = a " + imc + " compare o com os dados abaixo.";
+     elementValueResult.innerHTML= resultImc;
+ 
+    }
